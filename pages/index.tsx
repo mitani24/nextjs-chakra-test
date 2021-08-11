e54e60a05523c0bb4e1cf6e65f0fd26ad9ffa06f
mitani24 <h.mitani@ashita-team.com>
2021-08-11 16:33:17 +0900

    Home を適当に実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const [message, setMessage] = useState("hello");
+
+  return (
+    <Box w={500} p={3}>
+      <Input
+        placeholder="Basic usage"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <Text>{message}</Text>
+    </Box>
+  );
 }
